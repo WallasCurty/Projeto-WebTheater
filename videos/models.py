1db153ad4bbef6bbd973e87b777ca3c6965aa02c
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2022-06-30 19:28:15 -0300

    Estrutura classes
    Estrutura inicial

---
+++ videos/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Video(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    qtd_vizu = models.IntegerField()
+    video = models.URLField()
+    imagem = models.URLField()
+
+class Categoria(models.Model):
+    titulo_cat = models.CharField(max_length=100)
+    descricao_cat = models.TextField()
+    video_cat = models.URLField()
+    
